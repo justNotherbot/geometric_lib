27c46fcb14d8c9648d22260e9a58cbaedb38b3ca
unknown <tcookermann@gmail.com>
2025-12-05 02:02:15 +0300

    Update code

--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,27 @@
 import unittest
 
 def area(a: float, b: float):
+  '''
+		Returns area of a rectangle
+		Parameters:
+		  a,b (float): length of 2 perpendicular sides of the rectangle
+		Return value:
+		  area (float): area of the rectangle
+		Usage:
+		  r_area = area(side)
+	'''
   return a*b
 
 def perimeter(a: float, b: float):
+  '''
+		Returns perimeter of a rectangle
+		Parameters:
+		  a,b (float): length of 2 perpendicular sides of the rectangle
+		Return value:
+		  perimeter (float): perimeter of the rectangle
+		Usage:
+		  r_perimeter = perimeter(side)
+	'''
   return 2*(a+b)
 
 
--- triangle.py
+++ triangle.py
@@ -1,9 +1,28 @@
 import unittest
 
 def area(base: float, height: float):
+  '''
+		Returns area of a triangle
+		Parameters:
+		  base (float): length of the base of the triangle
+      height (float): length of the triangle's height(perpendicular to base)
+		Return value:
+		  area (float): area of the triangle
+		Usage:
+		  t_area = area(side)
+	'''
   return 0.5 * base * height
 
 def perimeter(a: float, b: float, c: float):
+  '''
+		Returns perimeter of a triangle
+		Parameters:
+		  a,b,c (float): length of three distinct sides of the triangle
+		Return value:
+		  perimeter (float): perimeter of the triangle
+		Usage:
+		  t_perimeter = perimeter(side)
+	'''
   return a+b+c
 
 class TriangleTestCase(unittest.TestCase):
